88a1a7daf41a2e074db5340f7810daefc9aca03c
Brian Velez <bvez94@gmail.com>
2019-10-03 13:07:19 -0700

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -3,15 +3,20 @@ import ReviewsContainer from '../../containers/ReviewsContainer'
 
 class Restaurant extends Component {
 
+  // handleOnClick = () => {
+  //   this.props.deleteRestaurant(this.props.restaurant.id)
+  // }
 
   render() {
     const { restaurant } = this.props;
+    
 
     return (
       <div>
         <li>
           {restaurant.text}
-          <button> X </button>
+          <button onClick={() => this.props.deleteRestaurant(restaurant.id)}> X </button>
+          {/* <button onClick={this.handleOnClick}> X </button> */}
           <ReviewsContainer restaurant={restaurant}/>
         </li>
       </div>
